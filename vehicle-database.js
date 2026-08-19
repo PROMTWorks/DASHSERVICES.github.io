67a38c9c1438117a159ee81015ff72e21c4a52e2
PROMTWorks <djroy20.2004@gmail.com>
2026-08-19 00:14:51 -0400

    Load 1984 NHTSA vehicle data

--- vehicle-database.js
+++ vehicle-database.js
@@ -6,8 +6,10 @@
   function load(src,done){var s=document.createElement('script');s.src=src;s.async=false;s.onload=done;s.onerror=function(){console.error('DASH vehicle database failed to load:',src);};document.head.appendChild(s);}
   function start(){
     load('./vehicle-customer-data.js?v=20260819a',function(){
-      load('./vehicle-database-expanded.js?v=20260819c',function(){
-        load('./vehicle-catalog.js?v=20260816b',function(){window.DASHVehicleDatabaseLoaded=true;});
+      load('./vehicle-customer-data-1984.js?v=20260819d',function(){
+        load('./vehicle-database-expanded.js?v=20260819c',function(){
+          load('./vehicle-catalog.js?v=20260816b',function(){window.DASHVehicleDatabaseLoaded=true;});
+        });
       });
     });
   }
