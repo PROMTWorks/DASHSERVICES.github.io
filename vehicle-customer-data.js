c4f5e96256458d3c60051777dc6594327beaeeeb
PROMTWorks <djroy20.2004@gmail.com>
2026-08-18 22:59:00 -0400

    Add customer-supplied 1952-1968 vehicle data

---
+++ vehicle-customer-data.js
@@ -0,0 +1,23 @@
+/* PROMT WORKS — CUSTOMER-SUPPLIED VEHICLE DATA */
+window.DASH_CUSTOMER_VEHICLES={
+  '1952':{'Ford':['LN800'],'Volkswagen':['Volkswagen']},
+  '1956':{'FABCO':['Wide Track'],'Ford':['Ford Truck and Van']},
+  '1960':{'Chevrolet':['Chevrolet Truck','Corvette'],'Ford':['Ford Truck and Van','Thunderbird'],'GMC':['5000','5500','6000','6500','GMC','S5000']},
+  '1964':{'Blue Bird':['All American'],'Cadillac':['DeVille'],'Chevrolet':['Chevrolet Truck','Corvair','Corvette','Malibu'],'Dodge':['D100'],'GMC':['5000','5500','6000','6500','GMC','PD4106','PD4107','S5000','S6M','S8M','SDH','SDM','T6H','TDH','TDM'],'Mercedes-Benz':['600'],'Plymouth':['Belvedere']},
+  '1968':{
+    'Alfa Romeo':['Giulia 1300 TI','Giulia Super','Giulia TI','GT','GT Junior','GT Veloce','Spider'],
+    'American Motors':['68 Series','Ambassador','American Motors','AMX','Javelin','Rebel'],'Avanti':['Avanti II'],
+    'Blue Bird':['All American','Wanderlodge','Wonderlodge'],'BMW':['1600-2','2002'],'Buick':['Skylark','Special'],'Cadillac':['Eldorado'],
+    'Chevrolet':['Belair','Biscayne','Brookwood','C Series','C10','C40','C50','C60','CA40','CA50','CA60','Camaro','Caprice','Chevelle','Chevrolet Truck','Chevy II','Corvair','Corvette','El Camino','G Series','H70','H80','Impala','J70','J80','Kingswood','Nova','P Series','S40','S50','S60','SE40','SE50','SE60','SS40','SS50','SS60','TE500','TE600','Townsman','TV70'],
+    'Dodge':['A100','C1000','C500','C600','C700','C800','Charger','CN900','CNT900','Coronet','CT700','CT800','CT850','CT900','D Series','D100','D300','D400','D500','D600','D700','D800','Dart','Dodge','Dodge Truck','L600','L700','LNT1000','LVT1000','M300','M375','Monaco','P200','P300','P375','Polara','S400','S500','S550','S600','W Series','W100','W200','W300','W400','W600','WM300'],
+    'FIAT':['124','850','850 Coupe','850 Spider'],
+    'Ford':['B Series','B500','B600','B700','B750','Bronco','Country Squire','Econoline','F Series (Light)','F Series (Medium)','F100','F150','F250','F350','F500','F600','F700','F750','Fairlane','Falcon','Ford','Ford Truck','Ford Truck and Van','L Series (Heavy)','LTD','Maverick','Montego','Mustang','P500','P550','P600','Ranchero','Ranger','Thunderbird','XL'],
+    'GMC':['C Series','C9500','E4500','E5500','E6500','EG','EG5500','EG6500','EM','EM4500','EM6500','ES5500','ES6500','G Series','GMC','H7500','H8500','H9500','J7500','J8500','J9500','M9500','P Series','PD4106','PD4107','Pickup','S4500','S5500','S6500','S6M','S8M','SDH','SDM','SG','SM','SM5500','SS5500','T6H','TDH','TDM','TV7500'],
+    'Honda':['CT90'],'Jaguar':['XKE'],'Jeep':['J Series'],
+    'Mercedes-Benz':['1013','1113','1313','220','230','230S','250','250S','300SEL'],
+    'Nissan':['510','L520','L521','Nissan','SPL311','SRL311'],'Oldsmobile':['88','98','F85','Oldsmobile','Toronado'],
+    'Plymouth':['Barracuda','Belvedere','Fury','Plymouth','Satellite','Valiant'],'Pontiac':['Firebird','GTO','LeMans','Pontiac','Tempest'],
+    'Rolls-Royce':['Bentley','Silver Shadow'],'SAAB':['95','96','97'],'Toyota':['Corona','Crown','Land Cruiser'],
+    'Volkswagen':['115','Beetle','Karman Ghia','Type I','Type II','Type III','Volkswagen']
+  }
+};
