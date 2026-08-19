d4d55db2fdac9ad4365f2126daf3e7c501bfb5f9
PROMTWorks <djroy20.2004@gmail.com>
2026-08-19 01:49:43 -0400

    Bust vehicle database selector cache after model loading fix

--- vehicle-database.js
+++ vehicle-database.js
@@ -8,13 +8,11 @@
     load('./vehicle-customer-data.js?v=20260819a',function(){
       load('./vehicle-customer-data-1984.js?v=20260819d',function(){
         load('./vehicle-customer-data-1988.js?v=20260819e',function(){
-          load('./vehicle-database-expanded.js?v=20260819g',function(){
+          load('./vehicle-database-expanded.js?v=20260819h',function(){
             load('./vehicle-catalog.js?v=20260816b',function(){
               window.DASHVehicleDatabaseLoaded=true;
               load('./vehicle-service-policy.js?v=20260819a',function(){
-                load('./vehicle-booking.js?v=20260819g',function(){
-                  window.DASHVehicleBookingLoaded=true;
-                });
+                load('./vehicle-booking.js?v=20260819g',function(){window.DASHVehicleBookingLoaded=true;});
               });
             });
           });
